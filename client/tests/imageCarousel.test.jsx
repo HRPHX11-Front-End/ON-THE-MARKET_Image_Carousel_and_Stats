2032e215b46fce3a62e740f7a44fe0ed7fc75dac
Michael Doty <michaeldoty@outlook.com>
2020-08-14 16:23:25 -0700

    mvpf complete

--- client/tests/imageCarousel.test.jsx
+++ client/tests/imageCarousel.test.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Adapter from 'enzyme-adapter-react-16.1';
 import { shallow } from 'enzyme';
 import mockAxios from 'axios';
 import fetch from 'node-fetch';
